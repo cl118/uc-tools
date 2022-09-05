4349feb6bae2f58b2eb014116f1a6208e2a6e8cf
Chris Le <le.chrisv@gmail.com>
2022-09-05 02:37:44 -0400

    removed account link

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,26 +5,33 @@ const Navbar = () => {
   const {
     authState: {
       user: { username, firstName },
-      isAuthenticated },
-    logoutUser
+      isAuthenticated,
+    },
+    logoutUser,
   } = useContext(AuthContext);
 
   const logout = () => logoutUser();
   return (
-    <div className='w-full h-12 shadow-md px-4 bg-white'>
+    <div className='w-full h-12 px-4 bg-white shadow-md'>
       <div className='max-w-[1440px] mx-auto flex h-full justify-between items-center'>
         <div className='flex items-center'>
-          <a href='/'><h1 className='text-[#011F5B] text-2xl font-bold'>LGH UC TOOLS</h1></a>
+          <a href='/'>
+            <h1 className='text-[#011F5B] text-2xl font-bold'>LGH UC TOOLS</h1>
+          </a>
         </div>
-        <div className='flex gap-4 items-center'>
-          {username !== null ? <><p>Welcome, {firstName}</p>
-            <p>|</p></> : null}
-          <a
+        <div className='flex items-center gap-4'>
+          {username !== null ? (
+            <>
+              <p>Welcome, {firstName}</p>
+              <p>|</p>
+            </>
+          ) : null}
+          {/* <a
             href='/account'
-            className='font-semibold hover:text-gray-400 hover:border-b-2 border-gray-400'
+            className='font-semibold border-gray-400 hover:text-gray-400 hover:border-b-2'
           >
             Account
-          </a>
+          </a> */}
           {isAuthenticated ? (
             <button
               onClick={logout}
